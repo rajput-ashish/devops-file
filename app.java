36a2072a8c4ab96b8f15e75a302e217c25c9c85b
rajput-ashish <aashusingh9852@gmail.com>
2024-09-24 17:19:10 +0530

    update in file

--- app.java
+++ app.java
@@ -5,6 +5,22 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         // Creates a reader instance which takes
+        # adding some functions in feature-2
+public class SumNatural {
+
+    public static void main(String[] args) {
+
+        int num = 100, sum = 0;
+
+        for(int i = 1; i <= num; ++i)
+        {
+            // sum = sum + i;
+            sum += i;
+        }
+
+        System.out.println("Sum = " + sum);
+    }
+        # function is added
         // input from standard input - keyboard
         Scanner reader = new Scanner(System.in);
         System.out.print("Enter a number: ");
